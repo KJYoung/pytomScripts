e6ed963a13c7be0f38f4817d8f798f223eb8f09f
KJYoung <jykim157@snu.ac.kr>
2022-02-27 00:55:40 +0900

    1.34 - compact volume generator

--- 01_SHREC_volumeviewer.sh
+++ 01_SHREC_volumeviewer.sh
@@ -1,7 +1,4 @@
-#!/bin/sh
+# THIS IS NOT FOR THE EXECUTION!!
 
-$IMOD_DIR/IMOD-linux.sh
-for pdbid in "1s3x" "3qm1" "3gl1" "3h84" "2cg9" "3d2f" "1u6g" "3cf3" "1bxn" "1qvr" "4cr2" "5mrc"
-do
-    echo "/cdata/volumes/${pdbid}_200.mrc"
-done
+# Just in Command Line
+for pdbid in "1s3x" "3qm1" "3gl1" "3h84" "2cg9" "3d2f" "1u6g" "3cf3" "1bxn" "1qvr" "4cr2" "5mrc"; do imod /cdata/volumesCompact/${pdbid}.mrc; done
---
+++ README.md
@@ -0,0 +1,3 @@
+### now working with 03, 04.   
+
+### 20220227 00:50 - compact volume generator completed.   
